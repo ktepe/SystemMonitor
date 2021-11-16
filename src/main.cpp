b8e6b6a57eea9bab23e76c3d86e476292878ab5a
ktepe <ketepe@gmail.com>
2021-11-16 13:28:32 +0000

    main remove debug lines

--- src/main.cpp
+++ src/main.cpp
@@ -2,52 +2,9 @@
 #include "system.h"
 #include <iostream>
 
-//ket
-#include <vector>
-using std::cout;
-using std::endl;
-using LinuxParser::CPUStates;
-#include "format.h"
-//tek
-#define ORG 1
 int main() {
   
-  // original
-  #if ORG
   System system;
   NCursesDisplay::Display(system);
-  // original
-  #else
-  
-  // ket
-  System system;
-  Processor pro;
-  //cout << "in main mem util " << system.MemoryUtilization() << endl;
-  auto cpu_usage = LinuxParser::CpuUtilization();
-  cout<< "stat information "<< cpu_usage[CPUStates::kUser_] << endl;
 
-  while(!cpu_usage.empty()){
-    cout << "in main cpu util " << cpu_usage.back() << endl;
-    cpu_usage.pop_back();
-  }
-
-  cout << "cpu_usage " << system.Cpu().Utilization() << endl; 
-  // tek
-  cout << "cmd line : " << LinuxParser::Command(1) << endl; 
-  /*
-  auto pids=LinuxParser::Pids();
-  while (!pids.empty()){
-    cout << "in main cpu PIDs " << pids.back() << endl;
-    pids.pop_back();
-
-  } 
-  
-  cout << "in main cpu PIDs uptime " << Format::ElapsedTime(LinuxParser::UpTime(1)) << endl;
-  */
-  cout << "in main Uid " << LinuxParser::Uid(1) << endl;
-  cout << "in main Uid " << LinuxParser::User(1) << endl;
-  cout << "in main running " << LinuxParser::RunningProcesses() << endl;
-
-
-#endif
 }
